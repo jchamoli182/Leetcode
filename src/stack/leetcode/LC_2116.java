bb847086b1bad9ef7e911d560ec468bfc32a7d64
Jayesh <jchamoli182@gmail.com>
2025-01-12 21:38:14 +0530

    First Commit

---
+++ src/stack/leetcode/LC_2116.java
@@ -0,0 +1,46 @@
+package stack.leetcode;
+
+import java.util.Stack;
+
+public class LC_2116 {
+    // using stack
+    private String s;
+    private String locked;
+    public LC_2116(String s,String locked) {
+
+        this.s = s;
+        this.locked = locked;
+
+    }
+
+    public boolean Solve(){
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> openClose = new Stack<>();
+    
+        for(int i=0;i<s.length();i++){
+            if(locked.charAt(i) == '0'){
+                openClose.push(i);
+            }
+            else if (s.charAt(i) == '('){
+                open.push(i);
+            }else{
+                if(!open.empty()){
+                    open.pop();
+                }
+                else if(!openClose.empty()){
+                    openClose.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        while(!open.empty() && !openClose.empty() && open.peek() < openClose.peek()){
+            open.pop();
+            openClose.pop();
+        }
+
+        return open.empty();
+    }
+}
