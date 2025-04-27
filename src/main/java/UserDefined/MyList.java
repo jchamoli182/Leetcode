8322fd6e902912ec27aa949634f87161653081d8
Jayesh <jchamoli182@gmail.com>
2025-04-27 19:44:24 +0530

    classes

---
+++ src/main/java/UserDefined/MyList.java
@@ -0,0 +1,68 @@
+package UserDefined;
+public class MyList {
+    // creating a user deifned list which can add,remove,get,size,replace an integer.
+    private int size;
+    private  int n = 5;  // default capacity is 5;
+    private int[] arr = new int[n];
+    private int ptr = 0;
+
+    //return size of an list funtion.
+    public int Size(){
+        return size;  
+    }
+    //adding the element is done.
+    public void add(int x){
+        //check if size is equal to capacity because no more element can be addded,increase the size of the list.
+        if(size == n){
+            n = 2 * n; //double the capacity.
+            arr = Copy(arr,n).clone();
+        }
+        arr[ptr] = x;
+        ptr++;
+        size++;
+    }
+    //get the element at particular index.
+    public int get(int idx){
+        if(idx >= size){
+            return -1;   //if index is out of bound return -1;
+        }
+
+        for(int i=0;i<size;i++){
+            if(i == idx) return arr[i];
+        }
+        return -1;
+    }
+
+    //replace an element at particular index
+    public void replace(int idx,int x){
+        if(idx >= size){
+            return;   //if index is out of bound return empty;
+        }
+        for(int i=0;i<size;i++){
+            if(i == idx){
+                arr[i] = x;
+                break;
+            }
+        }    
+    }
+    //remove at specific index.
+    public void remove(int idx){
+        if(idx >= size) return;
+        for(int i=idx+1;i<size;i++){
+            arr[i-1] = arr[i];
+        }
+        size--;
+        ptr--;
+    }
+
+
+    //fill the  array with previous elements
+    private int[] Copy(int[] arr,int n){
+        int[] a = new int[n];
+        for(int i=0;i<arr.length;i++){
+            a[i] = arr[i];
+        }
+
+        return a;
+    }
+}
